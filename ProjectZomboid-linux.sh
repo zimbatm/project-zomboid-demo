0dff36c70a461d659a632fd68fc93d4acdd3f404
Jonas Pfenniger <jonas@pfenniger.name>
2011-06-20 14:01:28 +0100

    Moar Fixes

--- ProjectZomboid-linux.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-platform=linux
-
-exec java -Xmx512m -Djava.library.path=native/$platform -Dsun.java2d.noddraw=true -Dsun.awt.noerasebackground=true -Dsun.java2d.d3d=false -Dsun.java2d.opengl=false -Dsun.java2d.pmoffscreen=false -cp .:res:jar\lwjgl.jar:jar\lwjgl_test.jar:jar\lwjgl_util.jar:zombie.jar: zombie.FrameLoader
